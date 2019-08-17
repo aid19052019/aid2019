1bad4a0e5c13724273dee006b65e842835c3b240
Tedu <yefeng461@sina.com>
2019-08-17 17:53:55 +0800

    chang seller

--- store/seller/views.py
+++ store/seller/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def goods_store_view(request):
+    if request.method == 'GET':
+        return render(request,'seller/goods_store.html')
--- store/store/views.py
+++ store/store/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render
+
+
+def goods_store_view(request):
+    if request.method == 'GET':
+        return render(request,'seller/goods_store.html')
